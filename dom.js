848fe17b91bff3eb6b2979c9f04d1b683d85eda6
abishekcr7 <abishekravi422@gmail.com>
2022-11-11 20:03:49 +0530

    My second commit

---
+++ dom.js
@@ -0,0 +1,10 @@
+var headertitle=document.getElementById("main-header")
+headertitle.innerHTML='<h3>HELLO WORLD</h3>'
+headertitle.style.textAlign='Center'
+
+var header=document.getElementById('main-header')
+header.style.border='solid 5px black'
+
+var head=document.getElementById('add')
+head.innerHTML='<b>Add Items</b>'
+head.style.color='green'
