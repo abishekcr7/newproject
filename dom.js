9c27d8bf7e393c8fd9ba41484bc5223218150018
abishekcr7 <abishekravi422@gmail.com>
2022-11-13 16:41:56 +0530

    my third comit

--- dom.js
+++ dom.js
@@ -1,10 +1,6 @@
-var headertitle=document.getElementById("main-header")
-headertitle.innerHTML='<h3>HELLO WORLD</h3>'
-headertitle.style.textAlign='Center'
-
-var header=document.getElementById('main-header')
-header.style.border='solid 5px black'
-
-var head=document.getElementById('add')
-head.innerHTML='<b>Add Items</b>'
-head.style.color='green'
+var items=document.getElementsByClassName("list-group-item")
+console.log(items)
+items[2].style.backgroundColor='Green'
+for (let i=0;i<items.length;i++){
+    items[i].style.fontWeight="bold" 
+}
