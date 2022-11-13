de327e9f2388fa86c521f6a5cd054a82d8547d95
abishekcr7 <abishekravi422@gmail.com>
2022-11-13 17:39:57 +0530

    Fourth cmmit

--- dom.js
+++ dom.js
@@ -1,6 +1,17 @@
-var items=document.getElementsByClassName("list-group-item")
+// var items=document.getElementsByClassName("list-group-item")
+// console.log(items)
+// items[2].style.backgroundColor='Green'
+// for (let i=0;i<items.length;i++){
+//     items[i].style.fontWeight="bold" 
+// }
+
+
+var items=document.querySelectorAll(".list-group-item")
 console.log(items)
-items[2].style.backgroundColor='Green'
-for (let i=0;i<items.length;i++){
-    items[i].style.fontWeight="bold" 
+items[1].style.color="Green"
+
+var odds=document.querySelectorAll('.list-group-item:nth-child(odd)')
+console.log(odds)
+for (var i=0;i<=odds.length ; i++){
+    odds[i].style.backgroundColor='Green'
 }
