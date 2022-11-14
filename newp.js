e36a9b8dbbe44f2fc87e91778417a039d43f2050
abishekcr7 <abishekravi422@gmail.com>
2022-11-14 20:13:53 +0530

    obj local storage

--- newp.js
+++ newp.js
@@ -5,9 +5,17 @@ form.addEventListener('submit',storeDetails)
 function storeDetails(e){
     e.preventDefault()
     var name=document.getElementById('uname').value 
-    var age=document.getElementById('age').value 
     var gender=document.getElementById('gender').value 
+    var age=document.getElementById('age').value 
+    
     localStorage.setItem('Name',name)
     localStorage.setItem('Gender',gender)
     localStorage.setItem('age',age)
+
+    var userobj={
+        name:name,
+        Gender:gender,
+        age:age
+    }
+    localStorage.setItem('userinfo',JSON.stringify(userobj))
 }
